554c70884b4271896f0de45a4e9b2ca13d37b399
Shadow096 <m.rumak90@gmail.com>
2016-07-31 20:58:19 +0200

    New version(new host)

--- pokesniperhelper/Program.cs
+++ pokesniperhelper/Program.cs
@@ -13,13 +13,14 @@ using System.Web;
 using System.Windows.Forms;
 using Newtonsoft.Json;
 
+using WebSocketSharp;
+
+
 namespace pokesniperhelper
 {
     class Program
     {
-
-        static List<Pokemon> ToRemoveList = new List<Pokemon>();
-
+        static WebSocket ws;
 
         [DllImport("user32.dll")]
         public static extern int SetForegroundWindow(IntPtr hWnd);
@@ -27,136 +28,123 @@ namespace pokesniperhelper
         [STAThread]
         static void Main()
         {
-            List<Pokemon> pokelist;
-
+            Console.Title = "AutoTyper for PokeSniper2 by Shadow";
             Console.WriteLine("AutoTyper for PokeSniper2");
-            pokelist = LoadPokemons();
-            while (true)
+
+            ws = new WebSocket("ws://spawns.sebastienvercammen.be:49002/socket.io/?EIO=3&transport=websocket");
+            ws.OnMessage += Ws_OnMessage;
+            ws.OnOpen += Ws_OnOpen;
+            ws.OnError += Ws_OnError;
+            ws.OnClose += Ws_OnClose;
+
+            ws.Connect();
+
+            Console.ReadKey();
+           
+        }
+
+        private static void TypePokemon(string name, string lat, string lon)
+        {
+            Process[] processes = Process.GetProcessesByName("PokeSniper2");
+
+            foreach (Process proc in processes)
             {
-                try
-                {
-                    Console.WriteLine("Loading pokemons");
-                    pokelist = LoadPokemons();
-                }
-                catch
-                {
-                    Console.WriteLine("Cant load pokemons (Retrying)");
-                }
-                if(pokelist.Count > 0)
-                {
+                SetForegroundWindow(proc.MainWindowHandle);
+                SendKeys.SendWait(name);
+                //Thread.Sleep(500);
+                SendKeys.SendWait("{ENTER}");
+                //Thread.Sleep(1500);
 
-                    foreach (Pokemon pk2 in ToRemoveList)
+                SendKeys.SendWait(String.Format("{0},{1}", lat, lon));
+               // Thread.Sleep(500);
+                SendKeys.SendWait("{ENTER}");
+              //  Thread.Sleep(5000);
+                SendKeys.SendWait("{ENTER}");
+            }
+        }
+
+        
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            string msg = e.Data;
+            //Console.WriteLine("Message: " + e.Data);
+            if(msg== "3")
+                Console.WriteLine("Pong");
+            else if(msg.Contains("42"))
+            {
+                if (msg.Contains("helo"))
+                {
+                    dynamic response = JsonConvert.DeserializeObject(msg.Remove(0, 2));
+                    if (response != null)
                     {
-                        Pokemon pk = pokelist.Find(x => x.ToString() == pk2.ToString());
-                        if (pk != null)
+                        foreach (var pokemon in response[1])
                         {
-                            pokelist.Remove(pk);
-                        }
-                    }
-
-                    Console.WriteLine("New pokemons: " + pokelist.Count);
-
-
-                    Process[] processes = Process.GetProcessesByName("PokeSniper2");
-
-                    foreach (Process proc in processes)
-                    {
-                        foreach (Pokemon poks in pokelist)
-                        {
-                            Console.WriteLine(poks.ToString());
-                            SetForegroundWindow(proc.MainWindowHandle);
-                            SendKeys.SendWait(poks.name);
-                            Thread.Sleep(500);
-                            SendKeys.SendWait("{ENTER}");
-                            Thread.Sleep(1500);
+                            //pokemon.name
+                            //pokemon.lat
+                            //pokemon.lon
+                            //pokemon.IV
+                            //pokemon.channel
+                            //pokemon.userId
+                            //pokemon.server
                             
-                            SendKeys.SendWait(poks.coords);
-                            Thread.Sleep(500);
-                            SendKeys.SendWait("{ENTER}");
-                            Thread.Sleep(5000);
-                            SendKeys.SendWait("{ENTER}");
+                            Console.WriteLine(String.Format("Pokemon: {0} ({1},{2}) {3}", pokemon.name, pokemon.lat, pokemon.lon, pokemon.IV));
+                            TypePokemon((string)pokemon.name, (string)pokemon.lat, (string)pokemon.lon);
 
-
-                            ToRemoveList.Add(poks);
                         }
                     }
-
-                    
                 }
-                pokelist.Clear();
-                Thread.Sleep(5000);
+                else if(msg.Contains("poke"))
+                {
+                    dynamic response = JsonConvert.DeserializeObject(msg.Remove(0, 2));
+                    if (response != null)
+                    {
+
+                        //response[1].name
+                        //response[1].lat
+                        //response[1].lon
+                        //response[1].IV
+
+                        Console.WriteLine(String.Format("New pokemon: {0} ({1},{2}) {3}", response[1].name, response[1].lat, response[1].lon, response[1].IV));
+                        TypePokemon((string)response[1].name, (string)response[1].lat, (string)response[1].lon);
+
+                    }
+                }
             }
         }
 
-
-        public static List<Pokemon> LoadPokemons()
+        private static void Ws_OnOpen(object sender, EventArgs e)
         {
-            List<Pokemon> pokelist = new List<Pokemon>();
-
-            dynamic response = JsonConvert.DeserializeObject(Fetch("get", "http://pokesnipers.com/api/v1/pokemon.json"));
-            if (response != null)
-            {
-                foreach(var pokemon in response.results)
-                {
-                    Pokemon poke = new Pokemon((string)pokemon.id, (string)pokemon.name, (string)pokemon.coords, (string)pokemon.until, (string)pokemon.icon);
-                    pokelist.Add(poke);
-                }
-            }
-
-            return pokelist;
+            Console.WriteLine("Connected to server");
+            ws.Send("2probe");
+            ws.Send("5");
+            new Thread(() => PingTimer()).Start();
         }
 
-
-        private static string Fetch(string method, string url, NameValueCollection data = null)
+        private static void Ws_OnClose(object sender, CloseEventArgs e)
         {
-
-            HttpWebResponse response = Request(method.ToUpper(), url, data);
-            Stream responseStream = response.GetResponseStream();
-            if (responseStream == null)
-                return "";
-            else
-            {
-                StreamReader reader = new StreamReader(responseStream);
-                return reader.ReadToEnd();
-            }
+            Console.WriteLine("Disconnected from server " + e.Code);
         }
 
-        private static HttpWebResponse Request(string method, string url, NameValueCollection data = null)
+        private static void Ws_OnError(object sender, WebSocketSharp.ErrorEventArgs e)
         {
-            string postData = "";
-            if (method == "POST")
+            Console.WriteLine("Error: " + e.Message);
+        }
+
+       
+
+        private static void PingTimer()
+        {
+            while(true)
             {
-                foreach (string key in data.Keys)
-                {
-                    postData += String.Format("{0}={1}&", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(data[key]));
-                }
-                if (postData.Length > 0)
-                    postData = postData.Remove(postData.Length - 1);
-
+                Thread.Sleep(25000);
+                Console.WriteLine("Ping");
+                if (ws.IsAlive)
+                    ws.Send("2");
+                else
+                    break;
+                
             }
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Accept = "application/json, text/javascript;q=0.9, */*;q=0.5";
-            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-            request.Method = method;
-            request.UseDefaultCredentials = true;
-            //request.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.6) Gecko/20060728 Firefox/1.5";
-            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36";
-            request.Timeout = 30000;
-
-            if (method == "POST")
-            {
-                byte[] bytes = Encoding.UTF8.GetBytes(postData);
-                request.ContentLength = bytes.Length;
-
-                using (Stream stream = request.GetRequestStream())
-                {
-                    stream.Write(bytes, 0, bytes.Length);
-                }
-            }
-
-            return request.GetResponse() as HttpWebResponse;
-
         }
     }
 }
